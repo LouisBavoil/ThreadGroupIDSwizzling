100ac47acbe22b52fc6c1508d56adc5f379f270f
LouisBavoil <lbavoil@nvidia.com>
2024-02-29 11:17:29 +0100

    Add license

--- ThreadGroupTilingX.hlsl
+++ ThreadGroupTilingX.hlsl
@@ -1,3 +1,25 @@
+/*
+* Copyright (c) 2020-2024, NVIDIA CORPORATION. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+* DEALINGS IN THE SOFTWARE.
+*/
+
 // Normally argument "dipatchGridDim" is parsed through a constant buffer. However, if for some reason it is a
 // static value, some DXC compiler versions will be unable to compile the code.
 // If that's the case for you, flip DXC_STATIC_DISPATCH_GRID_DIM definition from 0 to 1.
